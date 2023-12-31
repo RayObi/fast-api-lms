91857a35d92b4ce631b773d04ac320e8ee504138
RayObi <raymondww90@gmail.com>
2023-12-30 17:10:32 -0800

    creating routers for api endpoints and adding courses and sections to api

--- main.py
+++ main.py
@@ -1,44 +1,12 @@
 from fastapi import FastAPI, Path
 from pydantic import BaseModel
 from typing import Optional, List
+from api import users,courses,sections
 
-app = FastAPI(
-    title= "Fast API LMS",
-    description= "LMS for managing students and courses",
-    version= "0.0.1",
-    terms_of_service= "",
-    contact= {
-        "name": "Ray",
-        "email": "raymondww90@gmail.com"
-
-    },
-    license_info= {
-        "name": "Tilak Maharashtra",
-    },
+app = FastAPI(  
 )
 
 
-users = []
-
-
-class User(BaseModel):
-    email: str
-    is_active: bool
-    bio: Optional[str]
-
-@app.get("/users", response_model=List[User])
-async def get_users():
-    return users
-
-
-
-
-@app.post("/users")
-async def create_user(user: User):
-    users.append(user)
-    return "Success"
-
-
-@app.get("/users/{id}")
-async def get_user(id: int = Path(..., description="The ID of the user you want to retrieve.", gt=2)):
-    return users[id]
+app.include_router(users.router)
+app.include_router(courses.router)
+app.include_router(sections.router)
